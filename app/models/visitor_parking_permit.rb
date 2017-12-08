5567fe318518f0c28f9e990b6c226ae0c2739aeb
Annie Latham <alatham0@chicagobooth.edu>
2017-12-08 07:27:11 +0000

    propped up

--- app/models/visitor_parking_permit.rb
+++ app/models/visitor_parking_permit.rb
@@ -14,8 +14,6 @@ class VisitorParkingPermit < ApplicationRecord
 
   validates :activation_code, :presence => true
 
-  validates :activation_code, :length => { :minimum => 12, :maximum => 12 }
-
   validates :activation_code, :inclusion => { :in => [ '2018vispass1', '2018vispass2', '2018vispass3', '2018vispass4' ]  }
 
   validates :expiration_date, :presence => true
