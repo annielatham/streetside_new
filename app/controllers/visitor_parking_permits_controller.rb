a477e2af11fbea6238ab249c918cd91094a35727
Raghu Betina <raghu@firstdraft.com>
2017-12-08 06:19:38 +0000

    Display only signed in users visitor_parking_permits on index

--- app/controllers/visitor_parking_permits_controller.rb
+++ app/controllers/visitor_parking_permits_controller.rb
@@ -10,7 +10,7 @@ class VisitorParkingPermitsController < ApplicationController
   end
 
   def index
-    @visitor_parking_permits = VisitorParkingPermit.all
+    @visitor_parking_permits = current_user.visitor_parking_permits.page(params[:page]).per(10)
 
     render("visitor_parking_permits/index.html.erb")
   end
