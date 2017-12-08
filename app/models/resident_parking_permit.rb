4a48c386e076e23706ed2becfd7fa60af7c873e0
Raghu Betina <raghu@firstdraft.com>
2017-12-08 06:18:54 +0000

    Validate uniqueness of resident_parking_permits.resident_id

--- app/models/resident_parking_permit.rb
+++ app/models/resident_parking_permit.rb
@@ -5,6 +5,8 @@ class ResidentParkingPermit < ApplicationRecord
 
   # Validations
 
+  validates :resident_id, :uniqueness => { :scope => [:vehicle_id] }
+
   validates :resident_id, :presence => true
 
   validates :valid_date, :presence => true
