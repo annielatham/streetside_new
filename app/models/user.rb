31c7caa9e0018344c38a6297f60702c7f1ad603f
Raghu Betina <raghu@firstdraft.com>
2017-12-08 06:18:54 +0000

    Validate presence of users.address

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :address, :presence => true
+
   validates :first_name, :presence => true
 
   validates :last_name, :presence => true
